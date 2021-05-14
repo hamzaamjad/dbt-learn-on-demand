97e5ff7adcc2c183ef508f2986979ff29145bf9d
Hamza Amjad <hamza.azhar.amjad@gmail.com>
2021-05-14 17:33:15 +0000

    UPDATE - on-demand training - macros

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -8,6 +8,7 @@ WITH orders as (
 
     from {{source('jaffle_shop', 'orders')}}
     -- completed, shipped, returned, placed, return_pending
+    {{ limit_data_in_dev('order_date', 1000) }}
 )
 
 SELECT * FROM orders
